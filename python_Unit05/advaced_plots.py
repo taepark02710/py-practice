9f08764e56277e133be0f2bc502afa57f4f29500
KevinPark020 <103702700+KevinPark020@users.noreply.github.com>
2026-05-20 17:25:34 +0900

    Add grade plotting and CSV helper functions
    Add advaced_plots.py to plot individual student grades and class averages using the plotter module (plot_grades, get_average, plot_class_average, main). Update csv_fun.py to import csv, fix print spacing, and add CSV-based helpers: names_and_addresses_csv and average(), plus minor changes to main to demonstrate average(column 28) and correct the if __name__ == "__main__" guard. These changes add CSV parsing and basic plotting utilities for grade data.

---
+++ python_Unit05/advaced_plots.py
@@ -0,0 +1,57 @@
+import plotter
+import csv
+import re
+
+def plot_grades(filename, first_name, last_name):
+    plotter.init(first_name + " " + last_name, "Grade Item", "Score")
+    
+    with open(filename) as csv_file:
+        next(csv_file)
+        csv_reader = csv.reader(csv_file)
+        for record in csv_reader:
+            if re.findall(first_name, record[0]):
+                for index in range(3, len(record)):
+                    plotter.add_data_point(float(record[index]))
+                    
+    plotter.plot()
+    
+
+def get_average(filename, col):
+    sum = 0
+    cnt = 0
+    
+    with open(filename) as csv_file:
+        next(csv_file)
+        csv_reader = csv.reader(csv_file)
+        for record in csv_reader:
+            try:
+                grade = float(record[col])
+                sum += grade
+                cnt += 1
+            except: 
+                sum += 0
+    
+    return sum / cnt
+
+def plot_class_average(filename):
+    index = 3
+    plotter.init("Class Average (" + filename + ")",  "Grade Item", "Average")
+    
+    try: 
+        while True:
+            avg = get_average(filename, index)
+            plotter.add_data_point(avg)
+            index += 1
+    except:
+        plotter.plot()
+            
+                    
+                
+def main():
+    # plot_grades("data/full_grades_010.csv", "Sion", "Lobasso")
+    
+    plot_class_average("data/full_grades_999.csv")
+    input("Enter to quit...")
+    
+if __name__ == "__main__":
+    main()
--- python_Unit05/csv_fun.py
+++ python_Unit05/csv_fun.py
@@ -1,3 +1,5 @@
+import csv
+
 
 def opener(filename):
     try: 
@@ -15,16 +17,41 @@ def names_and_addresses(filename):
             fields = records.split(",")
             name = fields[0]
             address = fields[1]
-            print("name: " + name + "address: " + address)
+            print("name: " + name + " address: " + address)
 
-        
+    
+def names_and_addresses_csv(filename):    
+    with open(filename) as csv_file:
+        next(csv_file)
+        csv_reader = csv.reader(csv_file)
+        for record in csv_reader:
+            print(print("name: " + record[0] + " address: " + record[1]))
+  
+def average(filename, col):
+    sum = 0
+    cnt = 0
+    
+    with open(filename) as csv_file:
+        next(csv_file)
+        csv_reader = csv.reader(csv_file)
+        for record in csv_reader:
+            grade = record[col]
+            sum += float(grade)
+            cnt += 1
+            
+    avg = sum / cnt
+    return avg
+    
 
 def main():
     filename = input("Enter the filename: ")
     # is_opened = opener(filename)
-    names_and_addresses(filename)
-
-if __name__ == "__main()__":
+    # names_and_addresses(filename)
+    # names_and_addresses_csv(filename)
+    print(average(filename, 28))
+    
+    
+if __name__ == "__main__":
     main()
         
     
