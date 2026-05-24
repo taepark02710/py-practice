c5c0a686427103d6f9b630e3a28006d50a45d8b4
KevinPark020 <103702700+taepark02710@users.noreply.github.com>
2026-05-24 21:30:59 +0900

    Add card deck utilities and demo
    Implement card utilities: make_card (creates card tuple with name and ANSI-colored shorthand, maps face ranks to names), make_deck (builds standard 52-card deck), shuffle (in-place Fisher–Yates style shuffle), draw (pop from deck into a hand), and deal (deal alternating cards to two hands). Includes a simple main() demo that shuffles a deck and prints two 5-card hands. Adds random import and basic ANSI coloring for suits.

--- python_Unit08/cards.py
+++ python_Unit08/cards.py
@@ -1,3 +1,94 @@
+import random
+
 def make_card(rank, suit):
+    
+    shorthand = ""
+    
+    if rank < 10:
+        shorthand = " " + str(rank) + suit[0]
+    else:
+        shorthand = str(rank) + suit[0]
+        
+    if rank == 11:
+        rank = "Jack"
+        
+    if rank == 12:
+        rank = "Queen"
+    if rank == 13:
+        rank = "King"
+    
+    if rank == 14:
+        rank = "Ace"
+
+    name = str(rank) + " of " + suit
+    
+    white = "\033[37m"
+    red = "\033[31m"
+    blue = "\033[34m"
+    
+    if suit == "Hearts" or suit == "Diamonds":
+        shorthand = red + shorthand + white
+    else:
+        shorthand = blue + shorthand + white
+        
+    return (rank, suit, name, shorthand)
+
+def make_deck():
+    deck = []
+    ranks = [i for i in range(2, 15)]
+    suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+    for rank in ranks:
+        for suit in suits:
+            card = make_card(rank, suit)
+            deck.append(card)
+            
+    return deck
+
+def shuffle(deck):
+    
+    for i in range(0, len(deck)):
+        j = random.randint(i, len(deck) - 1)
+        temp = deck[i]
+        deck[i] = deck[j]
+        deck[j] = temp
+        
+    return deck
+
+def draw(deck, hand):
+    
+    if len(deck) <= 0:
+        return None
+    else:
+        card = deck.pop()
+        hand.append(card)
+    
+    return card
+
+def deal(deck, num):
+    hand1 = []
+    hand2 = []
+    
+    for _ in range(0, num):
+        draw(deck, hand1)
+        draw(deck, hand2)
+        
+    return hand1, hand2
+    
+def main():
+    # print(make_card(2, "Hearts")[3])
+    # print(make_card(10, "Diamonds")[3])
+    # print(make_card(14, "Spades")[3])
+    
+    # print(make_deck())
+    # print(len(make_deck()))
+    # print(shuffle(make_deck()))
+    print(deal(shuffle(make_deck()), 5)[0])
+    print(deal(shuffle(make_deck()), 5)[1])
+    
+    
+    
+if __name__ == "__main__":
+    main()
     
     
